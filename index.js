5de494cda78bed45b55e70cded43347db13b17e8
KyanosTower <hdmmjohnson@gmail.com>
2021-03-06 12:17:30 -0700

    still fixing path error

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
-app.get('/postal', (req, res) => res.render('pages/index'))
+app.get('/form', (req, res) => res.render('pages/index'))
 
 app.get("/getRate", (req, res) => {
     var total = calculateRate(req);
