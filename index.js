6d42d3fb04a6437b49a9641f482a38a4b1172de5
KyanosTower <hdmmjohnson@gmail.com>
2021-03-13 15:35:43 -0700

    fixing database connection

--- index.js
+++ index.js
@@ -10,27 +10,27 @@ var connection = mysql.createConnection({
     database: 'card'
 });
 
-connection.connect();
+app.get("/add", (req, res) => {
+    var cardD = "The card's name is " + req.query.cardName + ", it is from the series " + req.query.seriesName + " and it's rarity is " + req.query.rarity + ". Can it evolve? " + req.query.evolve + ".";
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = "INSERT INTO card (cardName, seriesName, rarity, evolve) VALUES ?";
+        var values = [
+            [req.query.cardName, req.query.seriesName, req.query.rarity, req.query.evolve]
+        ];
+        con.query(sql, [values], function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+        });
+    });
+    res.render("pages/results", { answer: cardD });
+});
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 app.get('/form', (req, res) => res.render('pages/index'))
-
-app.get("/add", (req, res) => {
-    var card = {
-        cardName: req.body.cardName,
-        seriesName: req.body.seriesName,
-        rarity: req.body.rarity,
-        evolve: req.body.evolve
-    }
-    var cardD = "The card's name is " + req.query.cardName + ", it is from the series " + req.query.seriesName + " and it's rarity is " + req.query.rarity + ". Can it evolve? " + req.query.evolve + ".";
-    connection.query('INSERT INTO card SET ?', card, function(err, resp) {
-        if (err) throw err;
-        res.send('<h1>Added to the Database<h1>');
-    });
-    res.render("pages/results", { answer: cardD });
-});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
