53e757357caad3865eaf541b6c55af060172b69e
KyanosTower <hdmmjohnson@gmail.com>
2021-03-20 17:08:17 -0600

    Still fixing path errors

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
-//const express = require('express');
+const express = require('express');
 const { createConnection } = require('net');
 const path = require('path');
 var mysql = require('mysql');
 const PORT = process.env.PORT || 5500
-//const app = express();
+const app = express();
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -35,7 +35,7 @@ app.get('/form', (req, res) => res.render('pages/index'))
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     host: 'localhost',
     database: 'card'
 });
@@ -54,4 +54,4 @@ function search(){
                 }
             });
     });
-}
+}*/
