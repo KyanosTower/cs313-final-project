8224a100e8f1b8484a79553023586d8aff73bb4e
KyanosTower <hdmmjohnson@gmail.com>
2021-03-20 18:24:18 -0600

    fixing more stuff

--- public/search.js
+++ public/search.js
@@ -1,9 +1,15 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    database: 'card'
+});
+
 function search(){
     var searchString = $('txtSearch').val();
     console.log('Searching for: ' + searchString);
 
     connection.connect(function(err) {
-            var sql = "SELECT cardName, seriesName, rarity, evolve FROM card WHERE name = ?";
+            var sql = "SELECT cardName, seriesName, rarity, evolve FROM 'card' WHERE name = ?";
             connection.query(sql, [searchString], function(err, result) {
                 if (result.Search && result.Search.length > 0){
                     var resultList = $('#ulResults');
