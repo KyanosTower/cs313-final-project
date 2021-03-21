b43d901deffc20f79d7ce09295842ee7ac888753
KyanosTower <hdmmjohnson@gmail.com>
2021-03-20 18:29:16 -0600

    fixing more stuff

--- index.js
+++ index.js
@@ -27,6 +27,21 @@ app.get("/add", (req, res) => {
     res.render("pages/results", { answer: cardD });
 });
 
+function search() {
+    var searchString = $('txtSearch').val();
+    console.log('Searching for: ' + searchString);
+
+    connection.connect(function (err) {
+        var sql = "SELECT cardName, seriesName, rarity, evolve FROM 'card' WHERE name = ?";
+        connection.query(sql, [searchString], function (err, result) {
+            if (result.Search && result.Search.length > 0) {
+                var resultList = $('#ulResults');
+                resultList.empty();
+            }
+        });
+    });
+}
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
