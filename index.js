3423008fd73c2ff49262a5d064f94811536896ab
KyanosTower <hdmmjohnson@gmail.com>
2021-03-20 21:51:03 -0600

    still working on AJAX

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/add", (req, res) => {
         var values = [
             [req.query.cardName, req.query.seriesName, req.query.rarity, req.query.evolve]
         ];
-        connection.query(sql, [values], function (err, result) {
+        connection.query(sql, values, function (err, result) {
             //if (err) throw err;
             console.log("1 record inserted");
         });
