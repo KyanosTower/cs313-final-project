3dd78ecda3e505f3d757dcdaddb12a737ed71319
KyanosTower <hdmmjohnson@gmail.com>
2021-03-20 22:13:03 -0600

    still working on AJAX

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ var connection = mysql.createConnection({
 app.get("/add", (req, res) => {
     var cardD = "The card's name is " + req.query.cardName + ", it is from the series " + req.query.seriesName + " and it's rarity is " + req.query.rarity + ". Can it evolve? " + req.query.evolve + ".";
     connection.connect(function (err) {
-        if (err) throw err
+        //if (err) throw err
         console.log('You are now connected...')
 
         connection.query("INSERT INTO card (cardName, seriesName, rarity, evolve) VALUES (?, ?, ?, ?)", [req.query.cardName, req.query.seriesName, req.query.rarity, req.query.evolve], function (err, result) {
-            if (err) throw err
+            //if (err) throw err
         })
     })
     res.render("pages/results", { answer: cardD });
