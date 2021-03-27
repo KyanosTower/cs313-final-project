552c44f0edf4daaf87bfd7628082c750a2b4f8c0
KyanosTower <hdmmjohnson@gmail.com>
2021-03-27 16:40:54 -0600

    working on sql stuff

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get("/add", (req, res) => {
         });
     })
     client.query('SELECT * FROM card', (err,res) => {
-        if (err) throw err;
+        //if (err) throw err;
         for (let row of res.row) {
             console.log(JSON.stringify(row));
         }
