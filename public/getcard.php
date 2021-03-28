d9e22654f0b366e6ef6d3aa9f631e0e100e9f87f
KyanosTower <hdmmjohnson@gmail.com>
2021-03-27 22:19:47 -0600

    working on sql stuff

--- public/getcard.php
+++ public/getcard.php
@@ -1,51 +1,57 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<style>
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
+  <style>
+    table {
+      width: 100%;
+      border-collapse: collapse;
+    }
 
-table, td, th {
-  border: 1px solid black;
-  padding: 5px;
-}
+    table,
+    td,
+    th {
+      border: 1px solid black;
+      padding: 5px;
+    }
 
-th {text-align: left;}
-</style>
+    th {
+      text-align: left;
+    }
+  </style>
 </head>
+
 <body>
 
-<?php
-$q = intval($_GET['q']);
+  <?php
+  $q = intval($_GET['q']);
 
-$con = mysqli_connect('localhost','username','password','card');
-if (!$con) {
-  die('Could not connect: ' . mysqli_error($con));
-}
+  $host='localhost';
+  $username='';
+  $password='';
+  $dbname='card';
 
-mysqli_select_db($con,"card");
-$sql="SELECT * FROM card;";
-$result = mysqli_query($con,$sql);
+  $con = mysqli_connect($host, $username, $password, $dbname);
+  if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+  }
 
-echo "<table>
-<tr>
-<th>Card Name</th>
-<th>Series Name</th>
-<th>Rarity</th>
-<th>Evolve?</th>
-</tr>";
-while($row = mysqli_fetch_array($result)) {
-  echo "<tr>";
-  echo "<td>" . $row['cardName'] . "</td>";
-  echo "<td>" . $row['seriesName'] . "</td>";
-  echo "<td>" . $row['rarity'] . "</td>";
-  echo "<td>" . $row['evolve'] . "</td>";
-  echo "</tr>";
-}
-echo "</table>";
-mysqli_close($con);
-?>
+  mysqli_select_db($con, "card");
+  $sql = "SELECT (cardName, seriesName, rarity, evolve) FROM card";
+  $result = mysqli_query($con, $sql);
+
+  echo "<table><tr><th>Card Name</th><th>Series Name</th><th>Rarity</th><th>Evolve?</th></tr>";
+  while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['cardName'] . "</td>";
+    echo "<td>" . $row['seriesName'] . "</td>";
+    echo "<td>" . $row['rarity'] . "</td>";
+    echo "<td>" . $row['evolve'] . "</td>";
+    echo "</tr>";
+  }
+  echo "</table>";
+  mysqli_close($con);
+  ?>
 </body>
+
 </html>
